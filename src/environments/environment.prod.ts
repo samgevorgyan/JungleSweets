3796f01d66a6a4e60a58042d565c46a53707b9b6
Samvel Gevorgyan <samvelgevorgyan87@gmail.com>
2021-03-06 20:42:27 +0100

    courses

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,6 @@ export const environment = {
     appId: '1:821964923546:web:6a3c8cbbfef9695cd73523',
     measurementId: 'G-PDDWVBMJHB',
   },
-  ssrUrl: 'https://www.junglesweets.com/',
+  // ssrUrl: 'https://www.junglesweets.com/',
+  ssrUrl: 'https://samgevorgyan.github.io/JungleSweets/',
 };
