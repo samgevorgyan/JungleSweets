2af7a1e43a53ee8e24df487defa2402800a626a1
Samvel Gevorgyan <samvelgevorgyan87@gmail.com>
2021-03-09 21:52:16 +0100

    courses end  html

---
+++ src/app/pages/courses/force-buy/force-buy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jungle-force-buy',
+  templateUrl: './force-buy.component.html',
+  styleUrls: ['./force-buy.component.scss']
+})
+export class ForceBuyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
